ac76d8988d5e08f2c2969181359e056eaac392c5
infoplat <infoplat@163.com>
2016-01-23 00:15:44 +0800

    chapter 3

---
+++ ipython_log.py
@@ -0,0 +1,165 @@
+# IPython log file
+
+result
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+result
+result
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+result
+result
+result
+result
+result
+result
+result
+result
+result
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+result
+result
+result
+result
+result
+result
+result
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+for i in range(50):
+    plot(randn(1000).cumsum())
+   
+    plot(randn(1000).cumsum())
+get_ipython().magic(u'paste')
+for i in range(500):
+    plot(randn(5000).cumsum())
+    
+for i in range(500):
+    plot(randn(1000).cumsum())
+    
+for i in range(500):
+    plot(randn(1000).cumsum())
+    
+for i in range(1000):
+    plot(randn(1000).cumsum())
+    
+2 ** 27
+_
+__
+_i139
+_139
+exec _i27
+a = "print 'hello world'"
+exex a
+exec a
+get_ipython().magic(u'pinfo a')
+a[-1:]
+get_ipython().magic(u'xdel a')
+a
+get_ipython().magic(u'')
+get_ipython().magic(u'logstart')
